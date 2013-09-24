e16ea6ded906e7695ed3ab0de49e760e11da73ea
crstjean <crstjean@localhost>
2013-09-24 12:38:48 -0400

    added image of chart for average response times

--- prog2/SymbolSet.cpp
+++ prog2/SymbolSet.cpp
@@ -203,3 +203,220 @@ void SymbolSet::draw()
     PrintText::drawStrokeText(toStr((trialNumber / NUM_TRIALS) + 1) + "/10 Targets", 0.5, 0.6, 0.35);
     PrintText::drawStrokeText(toStr((trialNumber % NUM_TRIALS) + 1) + "/30 Trials", 0.5, 0.1, 0.35);
 }
+/*
+void SymbolSet::draw() {
+  	glPushMatrix();
+	glTranslatef(12.0,5.0,0.0);
+
+    float x = 0.75;
+    float yScale = 2.5;
+
+    glPolygonMode( GL_FRONT, GL_FILL );   
+    glColor3f(1,1,1);
+    glRectf(-2, -1,  14, 12);
+
+    glPolygonMode(GL_FRONT, GL_LINE);
+    glColor3f(0.6, 0.6, 0.6);
+
+    glRectf(0, 0, 12, 0.5 * yScale);
+    glRectf(0, 0, 12, 1 * yScale);
+    glRectf(0, 0, 12, 1.5 * yScale);
+    glRectf(0, 0, 12, 2 * yScale);
+    glRectf(0, 0, 12, 2.5 * yScale);
+    glRectf(0, 0, 12, 3 * yScale);
+    glRectf(0, 0, 12, 3.5 * yScale);
+
+    
+    glRectf(0, 0, 4, 3.5 * yScale);
+    glRectf(0, 0, 8, 3.5 * yScale);
+
+    glColor3f(0, 0, 0);
+    PrintText::drawStrokeText("Target Trials", 2, -0.75, 0.45, HORIZ_CENTER, VERT_BOTTOM, false, 0);
+    PrintText::drawStrokeText("Blank Trials", 6, -0.75, 0.45, HORIZ_CENTER, VERT_BOTTOM, false, 0);
+    PrintText::drawStrokeText("All Trials", 10, -0.75, 0.45, HORIZ_CENTER, VERT_BOTTOM, false, 0);
+
+    PrintText::drawStrokeText("0", -0.25, 0 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("0.5", -0.25, 0.5 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("1.0", -0.25, 1.0 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("1.5", -0.25, 1.5 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("2.0", -0.25, 2.0 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("2.5", -0.25, 2.5 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("3.0", -0.25, 3.0 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+    PrintText::drawStrokeText("3.5", -0.25, 3.5 * yScale, 0.40, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+
+    PrintText::drawStrokeText("Time (seconds)", -1, 1.75 * yScale, 0.45, HORIZ_CENTER, VERT_BOTTOM, false, 90);
+
+    
+    PrintText::drawStrokeText("Average Response Times", 6, 3.75 * yScale, 0.55, HORIZ_CENTER, VERT_BOTTOM, false, 0);
+           
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.59066667, 0);
+        symbols[0]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.709733333, 0);
+        symbols[1]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.925266667, 0);
+        symbols[2]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.811066667, 0);
+        symbols[3]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.6674, 0);
+        symbols[4]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.873533333, 0);
+        symbols[5]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.261466667, 0);
+        symbols[6]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.750533333, 0);
+        symbols[7]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.201533333, 0);
+        symbols[8]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.717533333, 0);
+        symbols[9]->draw();
+    glPopMatrix();
+    x += 0.5;
+    glPushMatrix();
+        glTranslatef(x, yScale * 0.85087333, 0);
+        PrintText::drawStrokeText("*", 0, 0, 0.5, HORIZ_CENTER, VERT_CENTER, false, 0);
+    glPopMatrix();
+
+    x +=1.25;
+           
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.901133333, 0);
+        symbols[0]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.968066667, 0);
+        symbols[1]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.877933333, 0);
+        symbols[2]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.8682, 0);
+        symbols[3]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.1696, 0);
+        symbols[4]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 2.884933333, 0);
+        symbols[5]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 3.284933333, 0);
+        symbols[6]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.964133333, 0);
+        symbols[7]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 2.763533333, 0);
+        symbols[8]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.608133333, 0);
+        symbols[9]->draw();
+    glPopMatrix();
+    x += 0.5;
+    glPushMatrix();
+        glTranslatef(x, yScale * 1.92906, 0);
+        PrintText::drawStrokeText("*", 0, 0, 0.5, HORIZ_CENTER, VERT_CENTER, false, 0);
+    glPopMatrix();
+
+    x +=1.25;
+           
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.7459, 0);
+        symbols[0]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.8389, 0);
+        symbols[1]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.4016, 0);
+        symbols[2]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.339633333, 0);
+        symbols[3]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 0.9185, 0);
+        symbols[4]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.879233333, 0);
+        symbols[5]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 2.2732, 0);
+        symbols[6]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.357333333, 0);
+        symbols[7]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.982533333, 0);
+        symbols[8]->draw();
+    glPopMatrix();
+    x += 0.25;
+    glPushMatrix();
+	    glTranslatef(x, yScale * 1.162833333, 0);
+        symbols[9]->draw();
+    glPopMatrix();
+    x += 0.5;
+    glPushMatrix();
+        glTranslatef(x, yScale * 1.389966667, 0);
+        PrintText::drawStrokeText("*", 0, 0, 0.5, HORIZ_CENTER, VERT_CENTER, false, 0);
+    glPopMatrix();
+
+    glPopMatrix();
+}*/
